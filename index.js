b503d17e0e7417afa0dd14c0ccf54c3235583195
mghoubali <moha.g@hotmail.fr>
2019-08-29 10:53:24 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-    "use strict";
-   
+"use strict";
+
     var Game = {
         canvas : undefined,
         canvasContext : undefined,
@@ -9,54 +9,38 @@
     Game.start = function(){
         Game.canvas = document.getElementById("myCanvas");
         Game.canvasContext = Game.canvas.getContext("2d");
-        //debugger
-        Game.balloonSprite = new Image();
-        Game.balloonSprite.width = 50;
-        Game.balloonSprite.height = 50;
-
-        //debugger
-        Game.balloonSprite.src = './logo.png';
-
-        //console.log(Game.balloonSprite);
-        window.setTimeout(Game.mainLoop, 1000);
+        Game.balloonSprite = document.getElementById("logo");
+        /*new Image(100,100);
+        Game.balloonSprite.src = 'logo.png';
+        Game.balloonSprite.width = 100;
+        Game.balloonSprite.height = 100;*/
+        Game.mainLoop;
     };
 
     document.addEventListener('DOMContentLoaded', Game.start);
-  
+
     Game.draw = function(){
         Game.canvasContext.fillStyle = "yellow";
         Game.canvasContext.fillRect(Game.rectanglePosition, 100, 80, 50);
 
          //** I am assuming that this is where the problem is, if I take out this line of code the yellow box outlined above will appear, if this line is in the box will NOT appear**// 
-        var position = 
-        {
-          x : 50,
-          y : 50
-        }
-         Game.drawImage(Game.balloonSprite, {position});
-    };
 
-    
+        Game.drawImage(Game.balloonSprite, {x : 50, y : 50}); 
+    };
 
     Game.drawImage = function(sprite, position){
         Game.canvasContext.save();
         Game.canvasContext.translate(position.x, position.y);
-        debugger
-        Game.balloonSprite.onload = function() {
-          Game.canvasContext.drawImage(this, 50, 50);
-        };
+        sprite.onload = function(){
+          Game.canvasContext.drawImage(this,  50, 50, this.width, this.height,
+                                              50, 50, this.width, this.height);
+        }
 
-
-        //Game.canvasContext.drawImage(sprite,  0, 0);
+        
         Game.canvasContext.restore();
     };
 
     Game.mainLoop = function(){
         Game.draw();
     };
-    
-    Game.start();
-   
-    //Game.mainLoop();
-     //Game.drawImage( Game.balloonSprite, position);
     
