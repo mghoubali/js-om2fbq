2d18d8046d03fb0bbe42d28e41bc0d06d13f73cd
mghoubali <moha.g@hotmail.fr>
2019-08-29 09:25:47 +0200

    1 file modified

--- index.js
+++ index.js
@@ -9,31 +9,35 @@
         Game.canvas = document.getElementById("myCanvas");
         Game.canvasContext = Game.canvas.getContext("2d");
         //debugger
-        Game.balloonSprite = new Image();
+        Game.balloonSprite = new Image(100, 200);
+        //debugger
         Game.balloonSprite.src = './logo.png';
+
+        console.log(Game.balloonSprite);
         window.setTimeout(Game.mainLoop, 1000);
     };
 
     document.addEventListener('DOMContentLoaded', Game.start);
-
+  
     Game.draw = function(){
         Game.canvasContext.fillStyle = "yellow";
         Game.canvasContext.fillRect(Game.rectanglePosition, 100, 80, 50);
 
          //** I am assuming that this is where the problem is, if I take out this line of code the yellow box outlined above will appear, if this line is in the box will NOT appear**// 
-
+        var position = 
+        {
+          x : 50,
+          y : 50
+        }
          Game.drawImage(Game.balloonSprite, {position});
     };
 
-    var position = 
-    {
-      x : 50,
-      y : 50
-    }
+    
 
     Game.drawImage = function(sprite, position){
         Game.canvasContext.save();
         Game.canvasContext.translate(position.x, position.y);
+        debugger
         Game.canvasContext.drawImage(sprite,  0, 0, sprite.width, sprite.height,
                                               0, 0, sprite.width, sprite.height);
         Game.canvasContext.restore();
@@ -42,8 +46,9 @@
     Game.mainLoop = function(){
         Game.draw();
     };
+    
     Game.start();
    
-    Game.mainLoop();
-     Game.drawImage( Game.balloonSprite, position);
+    //Game.mainLoop();
+     //Game.drawImage( Game.balloonSprite, position);
     
